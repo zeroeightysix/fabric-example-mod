c612f17153a088025ffe2f3d0848fd8b63d7e220
zeroeightsix <RidanVandenbergh@gmail.com>
2019-12-19 18:22:14 +0100

    Use GLFW for key codes

--- src/main/java/net/fabricmc/example/mixin/MixinKeyboard.java
+++ src/main/java/net/fabricmc/example/mixin/MixinKeyboard.java
@@ -4,15 +4,14 @@ import net.fabricmc.example.ExampleMod;
 import net.fabricmc.example.ImguiScreen;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.TitleScreen;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(Keyboard.class)
-public class ExampleMixin {
+public class MixinKeyboard {
 
 	/*
 		This code is injected into minecraft code during runtime.
@@ -23,16 +22,22 @@ public class ExampleMixin {
 	@Inject(method = "onKey", at = @At(value = "RETURN", ordinal = 4), require = 1)
 	public void onKey(long window, int key, int scancode, int i, int j, CallbackInfo info) {
 		if (MinecraftClient.getInstance().player == null) return; // Don't do anything if not ingame
-		if (key == 89 && scancode == 29 && i != 0) { // the 'y' key. i == 0 if the key was released.
-			if (ExampleMod.imguiScreen == null) {
-				ExampleMod.imguiScreen = new ImguiScreen();
-			}
-			MinecraftClient.getInstance().openScreen(ExampleMod.imguiScreen);
-		} else if (key == 82 && scancode == 27 && i != 0) {
-			if (ExampleMod.imguiScreen != null) {
-				ExampleMod.imguiScreen.reload();
-			}
-		}
+
+		System.out.println(key);
+		System.out.println(scancode);
+
+		if (i != 0) {
+            if (key == GLFW.GLFW_KEY_Y) { // the 'y' key. i == 0 if the key was released.
+                if (ExampleMod.imguiScreen == null) {
+                    ExampleMod.imguiScreen = new ImguiScreen();
+                }
+                MinecraftClient.getInstance().openScreen(ExampleMod.imguiScreen);
+            } else if (key == GLFW.GLFW_KEY_R) {
+                if (ExampleMod.imguiScreen != null) {
+                    ExampleMod.imguiScreen.reload();
+                }
+            }
+        }
 	}
 
 }
